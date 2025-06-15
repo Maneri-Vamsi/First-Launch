b20bd31fbf664de6ada29e5ea903da31feb2108a
Maneri-Vamsi <manerivishnusaivamsi64@gmail.com>
2025-06-15 19:36:01 +0530

    fake_news_app.py

--- fake_news_app.py
+++ fake_news_app.py
@@ -155,3 +155,5 @@ def index():
 if __name__ == '__main__':
     print("🚀 Launching Fake News Detector on http://127.0.0.1:5000")
     app.run(debug=True, port=5000)
+torch.save(model.state_dict(), "model.pt")
+
